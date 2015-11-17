03e4b0f48d5f35f5b1c809c692fc5ddb1e0f7860
Marco <marco.brescianini@therope.it>
2015-11-17 16:33:13 +0100

    Initial Commit

---
+++ ESRestKitConfig/ESRestKitConfig.h
@@ -0,0 +1,19 @@
+//
+//  ESRestKitConfig.h
+//  ESRestKitConfig
+//
+//  Created by Engineering Solutions on 17/11/15.
+//  Copyright © 2015 Engineering Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESRestKitConfig.
+FOUNDATION_EXPORT double ESRestKitConfigVersionNumber;
+
+//! Project version string for ESRestKitConfig.
+FOUNDATION_EXPORT const unsigned char ESRestKitConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESRestKitConfig/PublicHeader.h>
+
+
